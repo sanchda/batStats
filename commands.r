60ab14fa6fc0046b01f1bc187e8ca3014970b86a
dasanchez <dasanchez@geisinger.edu>
2015-07-16 07:55:43 -0400

    Added regression

--- commands.r
+++ commands.r
@@ -14,6 +14,8 @@ physData = read.csv("EpomophorusPhysiology_2015.csv");
 rownames(physData) = physData$ID;  # Sets the rownames of the data frame, physData, to be the ID column of physdata
 physData$ID = NULL;                # Deletes the ID row of physData by setting it to NULL
 physData$sex = as.factor( ifelse( physData$class == "M_NS", "male", "female") );
+physData$disturbed = ifelse(physData$Habitat == "disturbed", 1, 0);
+physData$sexC = ifelse(physData$sex == "male", 1, 0);
 
 
 # Next, keep track of all of the variable names, except for Habitat and class
@@ -99,16 +101,48 @@ abline(physLm, col="black")
 #                    LINEAR REGRESSION (Male vs Female)
 ###############################################################################
 # Do the different linear models
-everyone.lm    = lm( Na ~ AnGap, physData );
-males.lm       = lm( Na ~ AnGap, physData[ physData$sex == "male", ] );
-females.lm     = lm( Na ~ AnGap, physData[ physData$sex == "female", ]  );
-disturbed.lm   = lm( Na ~ AnGap, physData[ physData$Habitat == "disturbed", ]  );
-undisturbed.lm = lm( Na ~ AnGap, physData[ physData$Habitat == "undisturbed", ]  );
+thisForm = "disturbed ~ Beecf";
+everyone.lm    = lm( as.formula(thisForm), physData );
+males.lm       = lm( as.formula(thisForm), physData[ physData$sex == "male", ] );
+females.lm     = lm( as.formula(thisForm), physData[ physData$sex == "female", ]  );
+disturbed.lm   = lm( as.formula(thisForm), physData[ physData$Habitat == "disturbed", ]  );
+undisturbed.lm = lm( as.formula(thisForm), physData[ physData$Habitat == "undisturbed", ]  );
 
-plot(Na ~ AnGap, physData, main="Na ~ AnGap", col=ifelse(physData$sex == "male", "blue", "red") )
+plot(PCO2 ~ pH, physData, main=thisForm, col=ifelse(physData$sex == "male", "blue", "red") )
 abline(everyone.lm, col="black")
 abline(males.lm,    col="blue")
 abline(females.lm,  col="red")
+
+
+plot(as.formula(thisForm), physData, main=thisForm, col=ifelse(physData$Habitat == "undisturbed", "blue", "red") )
+abline(everyone.lm, col="black")
+abline(undisturbed.lm,    col="blue")
+abline(disturbed.lm,  col="red")
+
+###############################################################################
+#                         Multiple Regression
+###############################################################################
+lm1DF = data.frame(dependent=NULL, independent=NULL, rsquared=NULL, P=NULL, multiR=NULL);
+
+thisForm_all  = as.formula("disturbed ~ BMI + Na + K + Cl + TCO2 + BUN + Glu + Hct + pH + PCO2 + HCO3 + Beecf + AnGap + Hb_star");
+
+thisForm      = as.formula("disturbed ~ Beecf");
+glm.out = glm( thisForm, family=binomial(logit), data=physData);
+
+
+add1(glm.out, thisForm_all ,test="Chisq")
+
+
+thisForm = as.formula("disturbed ~ Beecf + HCO3");
+glm.out = glm( thisForm, family=binomial(logit), data=physData);
+summary(glm.out)
+
+
+# Stepwise search; STRONGLY suggests that best fit for logistic regression is 3 or 2 variables.  So we're good!
+library(glmnet);
+glmmod = glmnet( x=as.matrix(physData[, numericCols]), y=physData$disturbed, alpha=1, family='binomial', nlambda=1000)
+cv.glmmod = cv.glmnet( x=as.matrix(physData[, numericCols]), y=physData$disturbed, alpha=1)
+plot( cv.glmmod )
 
 
 ###############################################################################
@@ -147,7 +181,9 @@ ggplot(physData, aes_string(x="Habitat", y=thisVar)) +
 #                   BAR CHART WITH STANDARD ERROR (Male vs Female)
 ###############################################################################
 # Only need to change this line.
-thisVar = "Na";
+plotVars = numericCols;
+for( thisVar in plotVars ) {
+
 thisData = summarySE(physData, measurevar=thisVar, groupvars=c("sex","Habitat"))
 thisData$quant = thisData[, thisVar];
 theseBreaks = seq( min(thisData$quant - 1.1*thisData$se), max(thisData$quant + 1.1*thisData$se), length.out=6);
@@ -171,11 +207,15 @@ ggplot(thisData, aes(x=sex, y=quant, fill=Habitat)) +
   coord_cartesian(ylim=c(min(thisData$quant - 1.1*thisData$se), max(thisData$quant + 1.1*thisData$se) ) ) +
   theme_bw() +
   theme(plot.title   = element_text(size=rel(2), face="bold")) +
-  theme(axis.title   = element_text(size=rel(1.5), face="bold")) +
-  theme(axis.text    = element_text(size=rel(1.5), face="bold", colour="blue")) +
-  theme(axis.ticks   = element_line(size=rel(2), colour = "black")) +
+  theme(axis.title   = element_text(size=rel(2), face="bold")) +
+  theme(axis.text    = element_text(size=rel(1), face="bold")) +
+  theme(axis.ticks   = element_line(size=rel(1), colour = "black")) +
   theme(legend.title = element_text(size=rel(2))) +
-  theme(legend.text  = element_text(size=rel(2)))
+  theme(legend.text  = element_text(size=rel(2)));
+
+ggsave(filename=paste("C:\\Users\\dasanchez\\dev\\batStats\\batStats\\sex_disturbance\\", thisVar, "_mf_distundist.png",sep=""),height=10,width=10);
+
+}
 
 
 
@@ -218,7 +258,7 @@ ggplot(thisData, aes(x=Habitat, y=quant, fill=sex)) +
 #                   BAR CHART WITH STANDARD ERROR (ONLY male v.s. female)
 ###############################################################################
 # Only need to change this line.
-thisVar = "BMI";
+thisVar = "Cl";
 thisData = summarySE(physData, measurevar=thisVar, groupvars=c("sex"))
 thisData$quant = thisData[, thisVar];
 theseBreaks = seq( min(thisData$quant - 1.1*thisData$se), max(thisData$quant + 1.1*thisData$se), length.out=6);
@@ -253,7 +293,7 @@ ggplot(thisData, aes(x=sex, y=quant, fill=sex)) +
 #              BAR CHART WITH STANDARD ERROR (ONLY disturbed vs undisturbed)
 ###############################################################################
 # Only need to change this line.
-thisVar = "BUN";
+thisVar = "PCO2";
 thisData = summarySE(physData, measurevar=thisVar, groupvars=c("Habitat"))
 thisData$quant = thisData[, thisVar];
 theseBreaks = seq( min(thisData$quant - 1.1*thisData$se), max(thisData$quant + 1.1*thisData$se), length.out=6);
@@ -267,7 +307,7 @@ ggplot(thisData, aes(x=Habitat, y=quant, fill=Habitat)) +
                 size=.3,    # Thinner lines
                 width=.2,
                 position=position_dodge(.9)) +
-  xlab("Sex") +
+  xlab("Habitat") +
   ylab(thisVar) +
   scale_fill_hue(name="Habitat type", # Legend label, use darker colors
                  breaks=c("disturbed", "undisturbed"),
@@ -276,11 +316,46 @@ ggplot(thisData, aes(x=Habitat, y=quant, fill=Habitat)) +
   scale_y_continuous(breaks=theseBreaks, labels=theseLabels  ) +
   coord_cartesian(ylim=c(min(thisData$quant - 1.1*thisData$se), max(thisData$quant + 1.1*thisData$se) ) ) +
   theme_bw() +
-  theme(plot.title   = element_text(size=rel(5), face="bold")) +
-  theme(axis.title   = element_text(size=rel(3), face="bold")) +
-  theme(axis.text    = element_text(size=rel(2), face="bold", colour="blue")) +
-  theme(axis.ticks   = element_line(size=rel(2), colour = "black")) +
-  theme(legend.title = element_text(size=rel(3))) +
+  theme(plot.title   = element_text(size=rel(2), face="bold")) +
+  theme(axis.title   = element_text(size=rel(2), face="bold")) +
+  theme(axis.text    = element_text(size=rel(1), face="bold", colour="black")) +
+  theme(axis.ticks   = element_line(size=rel(1), colour = "black")) +
+  theme(legend.title = element_text(size=rel(2))) +
+  theme(legend.text  = element_text(size=rel(2)))
+
+
+###############################################################################
+#              BAR CHART WITH STANDARD ERROR (ONLY males and disturbed vs undisturbed)
+###############################################################################
+# Only need to change this line.
+thisVar = "PCO2";
+thisData = summarySE(physData[physData$sex == "male",], measurevar=thisVar, groupvars=c("Habitat"))
+thisData$quant = thisData[, thisVar];
+theseBreaks = seq( min(thisData$quant - 1.1*thisData$se), max(thisData$quant + 1.1*thisData$se), length.out=6);
+theseLabels = sprintf("%.2f",theseBreaks );
+
+ggplot(thisData, aes(x=Habitat, y=quant, fill=Habitat)) + 
+  geom_bar(position=position_dodge(), stat="identity",
+           colour="black", # Use black outlines,
+           size=.3) +      # Thinner lines
+  geom_errorbar(aes( ymin=quant-se, ymax=quant+se ),
+                size=.3,    # Thinner lines
+                width=.2,
+                position=position_dodge(.9)) +
+  xlab("Habitat") +
+  ylab(thisVar) +
+  scale_fill_hue(name="Habitat type", # Legend label, use darker colors
+                 breaks=c("disturbed", "undisturbed"),
+                 labels=c("Disturbed", "Undisturbed")) +
+  ggtitle(paste("Mean ", thisVar, " with standard error", sep="") ) +
+  scale_y_continuous(breaks=theseBreaks, labels=theseLabels  ) +
+  coord_cartesian(ylim=c(min(thisData$quant - 1.1*thisData$se), max(thisData$quant + 1.1*thisData$se) ) ) +
+  theme_bw() +
+  theme(plot.title   = element_text(size=rel(2), face="bold")) +
+  theme(axis.title   = element_text(size=rel(2), face="bold")) +
+  theme(axis.text    = element_text(size=rel(1), face="bold", colour="black")) +
+  theme(axis.ticks   = element_line(size=rel(1), colour = "black")) +
+  theme(legend.title = element_text(size=rel(2))) +
   theme(legend.text  = element_text(size=rel(2)))
 
 
